aeb4fc3b5a5a3c4a236079645c5aa073060fea3e
Peter oyebamiji <peter2olumide@gmail.com>
2022-03-03 02:00:53 +0100

    final

---
+++ index.js
@@ -0,0 +1,24 @@
+let countEl = document.getElementById('count-el')
+
+let saveEl = document.getElementById('save-el')
+
+let count = 0;
+
+function increment(){
+    count += 1;
+    countEl.innerText = count;
+}
+
+function save(){
+    let entry = count + " -";
+    saveEl.innerText += " " + entry;
+    countEl.innerText = 0;
+    count = 0;
+
+
+}
+
+
+
+
+
